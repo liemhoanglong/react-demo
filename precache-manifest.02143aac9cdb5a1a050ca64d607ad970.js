8ef17c7041a228a116aa7bffb95a91cf01168201
Your Name <liemhoanglong@gmail.com>
2020-07-20 13:14:37 +0700

    Updates

---
+++ precache-manifest.02143aac9cdb5a1a050ca64d607ad970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86c9a4b8ed8ebed3a0a1ea3c60a7211",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "2dfeb9f3635271b77299",
+    "url": "/react-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "faf47e8a0afa599a0917",
+    "url": "/react-demo/static/js/2.29938b7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo/static/js/2.29938b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dfeb9f3635271b77299",
+    "url": "/react-demo/static/js/main.794a8c0e.chunk.js"
+  },
+  {
+    "revision": "9e95bcee3ee2cd82c94f",
+    "url": "/react-demo/static/js/runtime-main.f7dacf01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
